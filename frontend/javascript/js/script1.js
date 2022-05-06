07879c3c337b5691d31d87bd55ab937e0d2cf890
Nicole <nicolemaria030@hotmail.com>
2022-05-05 23:31:56 -0300

    Criação da tela Cartões

---
+++ frontend/javascript/js/script1.js
@@ -0,0 +1,14 @@
+class Cartao {
+    constructor() {
+        this.nome
+        this.bandeira
+        this.vencimento
+        this.valor
+    }
+    
+    adicionar() {
+        alert('Cartão adicionado.');
+    }
+}
+
+var cartao = new Cartao();
